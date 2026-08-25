32e78d6ff67423f2e57ca4c93e2212ab170ae0ce
you <kamadayou10@gmail.com>
2026-08-25 17:40:19 +0900

    [Update] 03 Roomスクリプトに敵生成位置情報の追加

--- RandomDungeon/Assets/WorkSpace/Script/Room.cs
+++ RandomDungeon/Assets/WorkSpace/Script/Room.cs
@@ -6,6 +6,7 @@ public class Room : MonoBehaviour
     [SerializeField] private GameObject wallRear;
     [SerializeField] private GameObject wallLeft;
     [SerializeField] private GameObject wallRight;
+    [SerializeField] private Transform[] enemySpawnPoints;
 
     public void SetWalls(bool hasFront,bool hasRear,bool hasLeft,bool hasRight)
     {
@@ -13,5 +14,9 @@ public class Room : MonoBehaviour
         wallRear.SetActive(!hasRear);
         wallLeft.SetActive(!hasLeft);
         wallRight.SetActive(!hasRight);
+    }
+    public Transform GetRandomEnemySpawnPoint()
+    {
+        return enemySpawnPoints[Random.Range(0, enemySpawnPoints.Length)];
     }
 }
