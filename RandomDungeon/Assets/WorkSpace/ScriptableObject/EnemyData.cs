4736775d73335c85925615e30ecee041e1e231de
you <kamadayou10@gmail.com>
2026-08-26 15:37:03 +0900

    [Update] 04 ScriptableObjectの更新

--- RandomDungeon/Assets/WorkSpace/ScriptableObject/EnemyData.cs
+++ RandomDungeon/Assets/WorkSpace/ScriptableObject/EnemyData.cs
@@ -9,8 +9,14 @@ public class EnemyData : ScriptableObject
     public float coolTime = 0;
     [Header("攻撃時間")]
     public float attackTime = 0;
+    [Header("ディレイ時間")]
+    public float delayTime = 0;
     [Header("攻撃距離")]
     public float attackDistance = 0;
-    [Header("移動距離")]
+    [Header("移動可能距離")]
     public float moveDistance = 0;
+    [Header("タグ名")]
+    public string tagName = "";
+    [Header("HP")]
+    public float hp = 0;
 }
--- RandomDungeon/Assets/WorkSpace/ScriptableObject/PlayerData.cs
+++ RandomDungeon/Assets/WorkSpace/ScriptableObject/PlayerData.cs
@@ -7,4 +7,14 @@ public class PlayerData : ScriptableObject
     public float moveSpeed = 0;
     [Header("マウス感度")] 
     public float mouseSensitivity = 0;
+    [Header("クールタイム")]
+    public float coolTime = 0;
+    [Header("攻撃時間")]
+    public float attackTime = 0;
+    [Header("ディレイ時間")]
+    public float delayTime = 0;
+    [Header("タグ名")]
+    public string tagName = "";
+    [Header("攻撃ダメージ")]
+    public float attackDamage = 0;
 }
