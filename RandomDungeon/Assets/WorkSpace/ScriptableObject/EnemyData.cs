76b274adfe3050fa181d8c757b724d1ac31aecf7
you <kamadayou10@gmail.com>
2026-08-27 15:01:14 +0900

    [Update] 05 ScriptableObjectの更新

--- RandomDungeon/Assets/WorkSpace/ScriptableObject/EnemyData.cs
+++ RandomDungeon/Assets/WorkSpace/ScriptableObject/EnemyData.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData")]
 public class EnemyData : ScriptableObject
 {
-    [Header("移動速度")]
-    public float moveSpeed = 0;
-    [Header("クールタイム")]
+    [Header("通常の敵の移動速度")]
+    public float enemyMoveSpeed = 0;
+    [Header("逃げる敵の移動速度")]
+    public float escapeEnemyMoveSpeed = 0;
+    [Header("追いかけ始める距離")]
+    public float moveDistance = 0;
+    [Header("逃げ始める距離")]
+    public float escapeDistance = 5f;
+    [Header("攻撃のクールタイム")]
     public float coolTime = 0;
     [Header("攻撃時間")]
     public float attackTime = 0;
-    [Header("ディレイ時間")]
+    [Header("攻撃までのディレイ時間")]
     public float delayTime = 0;
     [Header("攻撃距離")]
     public float attackDistance = 0;
-    [Header("移動可能距離")]
-    public float moveDistance = 0;
     [Header("タグ名")]
     public string tagName = "";
     [Header("HP")]
--- RandomDungeon/Assets/WorkSpace/ScriptableObject/StageData.cs
+++ RandomDungeon/Assets/WorkSpace/ScriptableObject/StageData.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 public class StageData : ScriptableObject
 {
     [Header("生成設定")]
-    public int roomCount = 10;
+    public int roomCount = 0;
 
     [Header("部屋のサイズ")]
-    public float roomSize = 5.0f;
+    public float roomSize = 0;
     [Header("敵の生成間隔")]
-    public int enemySpawnInterval = 3;
+    public int enemySpawnInterval = 0;
+    [Header("幅上限")]
+    public int maxWidth = 0;
+    [Header("高さ上限")]
+    public int maxHeight = 0;
+    [Header("最新の部屋を選ぶ確率")]
+    [Range(0.0f, 1.0f)]
+    public float latestRoomRate = 0;
+    [Header("逃げる敵を生成する最低距離")]
+    public int escapeEnemyMinDistance = 0;
 }
