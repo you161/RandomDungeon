0428b6cef1b1431d308c46a979a87280faa46ef8
you <kamadayou10@gmail.com>
2026-08-28 11:36:50 +0900

    [Update] 06 フェード中移動できないように更新

--- RandomDungeon/Assets/WorkSpace/Script/Player/PlayerManager.cs
+++ RandomDungeon/Assets/WorkSpace/Script/Player/PlayerManager.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
@@ -6,12 +5,31 @@ public class PlayerManager : MonoBehaviour
     [SerializeField] private PlayerMove playerMove = null;
     [SerializeField] private PlayerLook playerLook = null;
     [SerializeField] private PlayerAttack playerAttack = null;
-    [SerializeField] private bool canMove = false;
+    [SerializeField] private FadeManager fadeManager = null;
+
+    private bool previousCanMove;
 
     private void Start()
     {
-        playerMove.SetCanMove(canMove);
-        playerLook.SetCanMove(canMove);
-        playerAttack.SetCanMove(canMove);
+        SetPlayerCanMove(!fadeManager.GetIsFading());
+    }
+
+    private void Update()
+    {
+        bool currentCanMove = !fadeManager.GetIsFading();
+
+        if (currentCanMove != previousCanMove)
+        {
+            SetPlayerCanMove(currentCanMove);
+        }
+    }
+
+    private void SetPlayerCanMove(bool value)
+    {
+        previousCanMove = value;
+
+        playerMove.SetCanMove(value);
+        playerLook.SetCanMove(value);
+        playerAttack.SetCanMove(value);
     }
 }
